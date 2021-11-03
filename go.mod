522412ed2f5e7e88f53f2962c61347dd2693ef12
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-11-03 18:56:54 +0100

    add gorilla mux

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/lyx0/files
 
 go 1.17
+
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("xd")
+	r := mux.NewRouter()
+
+	http.HandleFunc("/", UploadHandler)
+
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         "127.0.0.1:8080",
+		WriteTimeout: 30 * time.Second,
+		ReadTimeout:  30 * time.Second,
+		IdleTimeout:  60 * time.Second,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+
+	signal.Notify(c, os.Interrupt)
+
+	<-c
+
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	srv.Shutdown(ctx)
+
+	log.Println("shutting down")
+	os.Exit(0)
+
+}
+
+func UploadHandler(rw http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
 }
