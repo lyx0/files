fe46d4571beb20554dd6e67b8262a4cf678ae7fc
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-11-10 22:26:52 +0100

    fix filenames not being random

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"html/template"
 	"io/ioutil"
+	"math/rand"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -16,13 +17,37 @@ func init() {
 }
 
 func main() {
-	http.HandleFunc("/", foo)
+	http.HandleFunc("/", upload)
+	// http.HandleFunc("/upload", upload)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.Handle("/uploads/", http.StripPrefix("/uploads/", http.FileServer(http.Dir("./uploads"))))
 	http.ListenAndServe(":8080", nil)
 }
 
-func foo(rw http.ResponseWriter, r *http.Request) {
+// func index(rw http.ResponseWriter, r *http.Request) {
+// 	tpl.ExecuteTemplate(rw, "index.gohtml", nil)
+// }
+
+// Create random string for the filename
+// https://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-go
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const (
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+)
+
+func RandStringBytesMask(n int) string {
+	b := make([]byte, n)
+	for i := 0; i < n; {
+		if idx := int(rand.Int63() & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i++
+		}
+	}
+	return string(b)
+}
+
+func upload(rw http.ResponseWriter, r *http.Request) {
 	var s string
 
 	if r.Method == http.MethodPost {
@@ -34,7 +59,7 @@ func foo(rw http.ResponseWriter, r *http.Request) {
 		}
 		defer f.Close()
 
-		fmt.Println("\nfile: ", "\nheader:", h, "\nerr:", err)
+		// fmt.Println("\nfile: ", f, "\nheader:", h, "\nerr:", err)
 
 		// Read file into memory
 		bs, err := ioutil.ReadAll(f)
@@ -42,10 +67,16 @@ func foo(rw http.ResponseWriter, r *http.Request) {
 			http.Error(rw, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		s = string(bs)
+		// fn = string(bs)
+
+		// Create filename
+		rnd := RandStringBytesMask(6)
+		// get file extension
+		ext := filepath.Ext(h.Filename)
+		s = fmt.Sprintf("%s%s", rnd, ext)
 
 		// Store file on disk
-		dest, err := os.Create(filepath.Join("./uploads/", h.Filename))
+		dest, err := os.Create(filepath.Join("./uploads/", s))
 		if err != nil {
 			http.Error(rw, err.Error(), http.StatusInternalServerError)
 			return
