bda8eafe450a5e47cccb3a73f432435646923452
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-11-10 21:13:42 +0100

    start over

--- handlers/types.go
+++
@@ -1,5 +0,0 @@
-package handlers
-
-import "html/template"
-
-var tpl *template.Template
--- main.go
+++ main.go
@@ -1,62 +1,35 @@
 package main
 
 import (
-	"context"
+	"fmt"
 	"html/template"
-	"log"
 	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
-
-	"github.com/lyx0/files/handlers"
 )
 
 var tpl *template.Template
 
 func init() {
-	tpl = template.Must(template.ParseGlob("templates/*.html"))
+	tpl = template.Must(template.ParseGlob("templates/*"))
 }
 
 func main() {
+	http.HandleFunc("/", foo)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
 
-	l := log.New(os.Stdout, "product-api", log.LstdFlags)
-	nu := handlers.NewUploader(l)
-	ni := handlers.NewIndex(l)
+func foo(rw http.ResponseWriter, r *http.Request) {
+	var s string
 
-	sm := http.NewServeMux()
-	sm.Handle("/upload", nu)
-	sm.Handle("/", ni)
-
-	s := &http.Server{
-		Addr:         ":8080",
-		Handler:      sm,
-		IdleTimeout:  120 * time.Second,
-		WriteTimeout: 15 * time.Second,
-		ReadTimeout:  15 * time.Second,
-	}
-
-	go func() {
-		err := s.ListenAndServe()
+	if r.Method == http.MethodPost {
+		// Open file/header from id "q"
+		f, h, err := r.FormFile("q")
 		if err != nil {
-			l.Fatal(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
-	}()
+		defer f.Close()
 
-	sigChan := make(chan os.Signal, 2)
-	signal.Notify(sigChan, os.Interrupt)
-	signal.Notify(sigChan, syscall.SIGTERM)
-
-	sig := <-sigChan
-	l.Println("Received terminate", sig)
-
-	tc, err := context.WithTimeout(context.Background(), 30*time.Second)
-	if err != nil {
-		l.Fatal(err)
+		fmt.Println("\nfile: ", "\nheader:", h, "\nerr:", err)
 	}
-	s.Shutdown(tc)
-
-	// Fix dumb warning
-	_ = tpl
 }
