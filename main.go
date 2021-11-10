a33e521527f3a526d31297b1a2d8236ac25b6dbb
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-11-10 21:23:42 +0100

    upload

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 var tpl *template.Template
@@ -25,11 +28,37 @@ func foo(rw http.ResponseWriter, r *http.Request) {
 		// Open file/header from id "q"
 		f, h, err := r.FormFile("q")
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
 			return
 		}
 		defer f.Close()
 
 		fmt.Println("\nfile: ", "\nheader:", h, "\nerr:", err)
+
+		// Read file into memory
+		bs, err := ioutil.ReadAll(f)
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		s = string(bs)
+
+		// Store file on disk
+		dest, err := os.Create(filepath.Join("./uploads/", h.Filename))
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer dest.Close()
+
+		_, err = dest.Write(bs)
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			return
+		}
 	}
+
+	rw.Header().Set("Content-Type", "text/html; charset=utf-8")
+	tpl.ExecuteTemplate(rw, "index.gohtml", s)
+
 }
